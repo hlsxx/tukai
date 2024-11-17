af8c1d4b13ee2c3e1f8164d37015ddc908c74a5f
hlsxx <holespato@gmail.com>
2024-11-17 16:06:27 +0100

    v0.0.2

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,7 @@
 use ratatui::{layout::Alignment, widgets::block::{Position, Title}};
 
 pub fn get_title(layout_name: &str, window_name: &str) -> Title<'static> {
-  Title::from(format!("⌨ tukai v0.0.1 》{} 》{} ⌨", layout_name, window_name))
+  Title::from(format!("⌨ tukai v0.0.2 》{} 》{} ⌨", layout_name, window_name))
     .position(Position::Top)
     .alignment(Alignment::Left)
 }
