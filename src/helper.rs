f2bb1405b04e8ab84d6f95ad54fc428f53298e84
hlsxx <holespato@gmail.com>
2024-11-21 13:00:09 +0100

    Layout and title

--- src/helper.rs
+++ src/helper.rs
@@ -8,7 +8,7 @@ use ratatui::{
 pub struct Generator;
 
 pub fn get_title(version: &String, layout_name: &str, window_name: &str) -> Title<'static> {
-  Title::from(format!("⌨ tukai v{} 》{} 》{} ⌨", version, layout_name, window_name))
+  Title::from(format!(" tukai v{} 》{} 》{} ", version, layout_name, window_name))
     .position(Position::Top)
     .alignment(Alignment::Left)
 }
